e96c01a8953130aa89ec23e472095e33514373c1
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2019-12-28 09:54:03 +0530

    Slider auto scroll fixed

--- TheEvent/js/slider.js
+++ TheEvent/js/slider.js
@@ -1,30 +1,32 @@
-var TIMEOUT = 6000;
- 
+var TIMEOUT = 3000;
+
+
 var interval = setInterval(handleNext, TIMEOUT);
- 
+
 function handleNext() {
- 
-  var $radios = $('input[class*=&quot;slide-radio&quot;]');
-  var $activeRadio = $('input[class*=&quot;slide-radio&quot;]:checked');
- 
+
+  var $radios = $('input[class*="slide-radio"]');
+  var $activeRadio = $('input[class*="slide-radio"]:checked');
+
   var currentIndex = $activeRadio.index();
   var radiosLength = $radios.length;
- 
+
   $radios
     .attr('checked', false);
- 
-  if (currentIndex &gt;= radiosLength - 1) {
- 
+
+
+  if (currentIndex >= radiosLength - 1) {
+
     $radios
       .first()
       .attr('checked', true);
- 
+
   } else {
- 
+
     $activeRadio
-      .next('input[class*=&quot;slide-radio&quot;]')
+      .next('input[class*="slide-radio"]')
       .attr('checked', true);
- 
+
   }
- 
+
 }
