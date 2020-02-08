73babed2c1d8f7371cc6e0d6a4a03dba865561a2
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2020-02-09 00:26:18 +0530

    Slider + Events

--- TheEvent/js/slider.js
+++ TheEvent/js/slider.js
@@ -1,32 +1,19 @@
-var TIMEOUT = 30000;
-
+var TIMEOUT = 20000;
 
 var interval = setInterval(handleNext, TIMEOUT);
 
 function handleNext() {
-
   var $radios = $('input[class*="slide-radio"]');
   var $activeRadio = $('input[class*="slide-radio"]:checked');
 
   var currentIndex = $activeRadio.index();
   var radiosLength = $radios.length;
 
-  $radios
-    .attr('checked', false);
-
+  $radios.attr("checked", false);
 
   if (currentIndex >= radiosLength - 1) {
-
-    $radios
-      .first()
-      .attr('checked', true);
-
+    $radios.first().attr("checked", true);
   } else {
-
-    $activeRadio
-      .next('input[class*="slide-radio"]')
-      .attr('checked', true);
-
+    $activeRadio.next('input[class*="slide-radio"]').attr("checked", true);
   }
-
 }
